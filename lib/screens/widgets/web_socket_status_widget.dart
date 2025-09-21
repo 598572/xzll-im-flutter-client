1d6312f4cc183bbde6b7e4e9dcf65aa3a6155b86
matkurban <3496354336@qq.com>
2025-09-21 17:19:46 +0800

    重构代码

---
+++ lib/screens/widgets/web_socket_status_widget.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+ 
+class WebSocketStatusWidget extends StatelessWidget {
+  const WebSocketStatusWidget({super.key});
+ 
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder(
+      child: Text('WebSocketStatusWidget'),
+    );
+  }
+}
